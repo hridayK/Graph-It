1b41a10bec2690b1b72215c876a8d42b83b30abd
Hriday Keswani <hridaykeswani@gmail.com>
2021-11-05 20:53:54 +0530

    made switching between form's state relatively smooth

--- bar/bar.js
+++ bar/bar.js
@@ -41,14 +41,6 @@ var chart = new Chart(ctx, {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            /*borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],*/
             borderWidth: 1
         }]
     },
@@ -91,7 +83,8 @@ var chart = new Chart(ctx, {
             del.style.display="none"
             edit.style.display="none"
             e_index=null;
-            count++;
+            if(count%2==0)
+              count++;
           }
         }
       },plugins:[{
