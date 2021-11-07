b8c92b4e86de041bd6558fa50bf0296254c4d69a
Hriday Keswani <hridaykeswani@gmail.com>
2021-11-07 09:30:34 +0530

    Implemented line chart and radar chart

--- line/line.js
+++ line/line.js
@@ -27,21 +27,22 @@ var canvasElement = document.getElementById("graph");
 var ctx = canvasElement.getContext("2d");
 
 var chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: [],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '',
-            data: [],
-            backgroundColor: [
+            data: [12, 19, 3, 5, 2, 3],
+            /*backgroundColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            ],*/
+            borderColor: "rgb(75, 192, 192)",
+            borderWidth: 2
         }]
     },
     options: {
--- radar/radar.js
+++ radar/radar.js
@@ -27,21 +27,18 @@ var canvasElement = document.getElementById("graph");
 var ctx = canvasElement.getContext("2d");
 
 var chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'radar',
     data: {
-        labels: [],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
             label: '',
-            data: [],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 3,
+            //pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
         }]
     },
     options: {
